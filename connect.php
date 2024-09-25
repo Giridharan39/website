a134c583a1f35425c63d2b39d2088cd3d3768bba
GIRIDHARAN G <126384652+Giridharan39@users.noreply.github.com>
2024-09-25 09:14:42 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+    $user=$POST['user']
+    $email =$POST['email']
+    $age =$POST['age']
+    $address =$POST['address']
+    $password =$POST['password']
+    $conpass =$POST['conpass']
+    //database connection
+ ?>
